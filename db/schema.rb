65499c0bf584b6ca61f06efe6d422c53fc1f25ac
EC2 Default User <ec2-user@ip-172-31-80-47.ec2.internal>
2022-05-12 08:02:43 +0000

    finish kadai2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_061927) do
+ActiveRecord::Schema.define(version: 2022_05_11_124200) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "body"
   end
 
 end
